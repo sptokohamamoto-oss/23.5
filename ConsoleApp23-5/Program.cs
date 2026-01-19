926333cc2f6de52545f43a1d598498d4d8874b76
冬桜子 濱元 <toko0124615@icloud.com>
2026-01-19 11:03:08 +0900

    a

--- ConsoleApp23-5/Program.cs
+++ ConsoleApp23-5/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+public interface IOutput
+{
+    void Write(string text);
+}
+
+public class ConsoleOutput : IOutput
+{
+    public void Write(string text)
+    {
+        Console.WriteLine(text);
+    }
+}
+
+public class FileOutput : IOutput
+{
+    private readonly string filePath;
+
+    public FileOutput(string filePath)
+    {
+        this.filePath = filePath;
+    }
+
+    public void Write(string text)
+    {
+        File.AppendAllText(filePath, text + Environment.NewLine);
+    }
+}
+
+public class ReportPrinter
+{
+    private readonly IOutput output;
+
+    public ReportPrinter(IOutput output)
+    {
+        this.output = output;
+    }
+
+    public void Print(string text)
+    {
+        output.Write(text);
+    }
+}
